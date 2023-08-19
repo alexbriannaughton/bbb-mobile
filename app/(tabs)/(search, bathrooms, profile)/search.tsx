7a0c505806e0698b8b29190be4b53da6e79f4e09
Evan House <evanahouse@gmail.com>
2023-08-18 18:30:37 -0700

    configuyred eas build

--- app/(tabs)/(search, bathrooms, profile)/search.tsx
+++ app/(tabs)/(search, bathrooms, profile)/search.tsx
@@ -18,7 +18,15 @@ export default function SearchScreen() {
       <ScreenWrapper>
         <Stack.Screen />
         <SView flex={1}>
-          <MapView style={{ width: "100%", height: "100%" }} />
+          <MapView
+            style={{ width: "100%", height: "100%" }}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
         </SView>
       </ScreenWrapper>
     </>
