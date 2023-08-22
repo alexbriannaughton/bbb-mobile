2047feb1a51bf39be733cf6be7d25128d356b189
Evan House <evanahouse@gmail.com>
2023-08-22 15:35:19 -0700

    fixed bathroom scren

--- app/(tabs)/(search, bathrooms, profile)/bathroom/[bathroom].tsx
+++ app/(tabs)/(search, bathrooms, profile)/bathroom/[bathroom].tsx
@@ -3,51 +3,55 @@ import { useEffect, useState } from "react";
 import { SText } from "../../../../components/SText";
 import { ScreenWrapper } from "../../../../components/ScreenWrapper";
 
+import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../../../lib/supabase-service";
 
 export default function ImageDetail() {
   const router = useRouter();
-  const { id } = useSearchParams();
+  const { bathroom } = useSearchParams();
   const [bathroomData, setBathroomData] = useState();
 
-//   (tabs)/(bathrooms)/bathrooms/234509872346509872345609873450
+  //   (tabs)/(bathrooms)/bathrooms/234509872346509872345609873450
 
   // const { isLoading, isError, data, error, isFetching, isPreviousData } =
-  // useQuery(["tasks", id], async () => {
-  //   const { data, error } = await supabase
-  //     .from("Tasks")
-  //     .select("*")
-  //     .eq("id", id);
+  //   useQuery(["bathrooms", id], async () => {
+  //     const { data, error } = await supabase
+  //       .from("bathrooms")
+  //       .select("*")
+  //       .eq("id", id);
 
   //     setBathroomData(data as any);
-  //   return data;
-  // });
+  //     return data;
+  //   });
 
   useEffect(() => {
     (async () => {
       const { data, error } = await supabase
         .from("bathrooms")
         .select("*")
-        .eq("id", id);
+        .eq("id", bathroom);
 
       setBathroomData(data as any);
       return data;
     })();
   }, []);
 
+  console.log("id", bathroom);
+
   return (
     <ScreenWrapper>
-      <Stack.Screen options={{ title: "Bathroom Detail", headerShown: true }} />
-      <SText>{JSON.stringify(id)}</SText>
-      <SText>{bathroomData && JSON.stringify(bathroomData[0], null, 2)}</SText>
-      <SText
-        onPress={() => {
-          // Go back to the previous screen using the imperative API.
-          router.back();
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerBackTitleStyle: {
+            fontFamily: "SpaceMono",
+          },
+          headerTitle: "",
         }}
-      >
-        GO BACK
-      </SText>
+      />
+      <SText variant="body">{JSON.stringify(bathroom)}</SText>
+      <SText variant="body">{bathroomData && JSON.stringify(bathroomData[0], null, 2)}</SText>
+    
     </ScreenWrapper>
   );
 }
